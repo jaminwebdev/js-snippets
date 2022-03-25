b7ab3a44b4e8f9c7a4a533a61066ddab6d294ea1
Jamin <jaminwebdev@gmail.com>
2022-03-24 17:24:30 -0700

    graph traversal methods added

--- graphs.js
+++ graphs.js
@@ -53,4 +53,103 @@ class Graph {
         delete this.addVertext[vertex];
 
     }
+
+    depthFirstRecursive(start) {
+        const result = [];
+        const visited = {};
+
+        const dfs = (vertex) => {
+            if(!vertex) {
+                return;
+            }
+
+            visited[vertex] = true;
+            result.push(vertex);
+
+            this.adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]) {
+                    return dfs(neighbor);
+                }
+            })
+        }
+
+        dfs(start);
+
+        return result;
+    }
+
+    depthFirstIterative(start) {
+        const stack = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex
+
+        visited[start] = true;
+
+        while(stack.length) {
+            currentVertex = stack.pop();
+
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    stack.push(neighbor);
+                }
+            })
+        }
+
+        return result;
+    }
+
+    breadthFirstSearch(start) {
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+
+        visited[start] = true;
+        
+        while(queue.length) {
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor] ) {
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            })
+        }
+
+        return result;
+    }
 }
+
+const myGraph = new Graph();
+
+myGraph.addVertext('A');
+myGraph.addVertext('B');
+myGraph.addVertext('C');
+myGraph.addVertext('D');
+myGraph.addVertext('E');
+myGraph.addVertext('F');
+
+myGraph.addEdge("A", "B");
+myGraph.addEdge("A", "C");
+myGraph.addEdge("B", "D");
+myGraph.addEdge("C", "E");
+myGraph.addEdge("D", "E");
+myGraph.addEdge("D", "F");
+myGraph.addEdge("E", "F");
+
+//        A
+//      /   \
+//     B     C
+//     |     | 
+//     D-----E
+//     \    /
+//        F
+
+
+console.log(myGraph.depthFirstRecursive("A"));
