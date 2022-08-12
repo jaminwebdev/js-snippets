9c1b6ba43f3dd910dc80d54426eff71c425adb3b
Jamin <jaminwebdev@gmail.com>
2022-08-12 11:22:36 -0700

    factory pattern

---
+++ pattern-factory.js
@@ -0,0 +1,22 @@
+// we can use factory functions in order to create new objects
+const createUser = ({ firstName, lastName, email }) => ({
+    firstName,
+    lastName,
+    email,
+    fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+});
+
+const user1 = createUser({
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@doe.com"
+});
+
+const user2 = createUser({
+    firstName: "Jane",
+    lastName: "Doe",
+    email: "jane@doe.com"
+});
+  
