94f9a139d332d1f6fd3b508df488068fca7700e5
Jamin <jaminwebdev@gmail.com>
2022-09-07 10:24:57 -0700

    first and last index problem

---
+++ firstAndLastIndex.js
@@ -0,0 +1,51 @@
+// given a sorted array
+// write a function that returns first and last position of a target
+
+const exampleArray = [2, 4, 5, 5, 5, 5, 5, 7, 9, 9];
+
+// becasue array is sorted, all items are adjacent to each other
+
+const loopFirstAndLast = (arr, target) => {
+    let start, finish
+    for (let [index, num] of arr.entries()) {
+        if (!start && num === target) {
+            start = index;
+            continue;
+        }
+
+        if (num === target) {
+            finish = index
+        }
+    }
+
+    if(!start) {
+        return [-1. -1]
+    }
+
+    return [start, finish]
+}
+
+// console.log(loopFirstAndLast(exampleArray, 5))
+
+// create lookup object w/target as key and array of indices as value
+// ex: { 5: [2, 3, 4, 5] }
+// return first and last index of lookup array
+// this will work for unsorted array as well
+const reduceLookupFistAndLast = (arr, target) => {
+    const targetIndexesObj = arr.reduce((lookup, num, index) => { 
+        if (num === target) {
+            lookup[target].push(index);
+        }
+        return lookup
+    }, {[target]: []})
+
+    const indices = targetIndexesObj[target]
+
+    if (!indices.length) {
+        return [-1, -1]
+    }
+
+    return [indices[0], indices[indices.length - 1]]
+}
+
+console.log(reduceLookupFistAndLast(exampleArray, 5))
