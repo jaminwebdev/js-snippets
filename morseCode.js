181410d32107d5501340de6b9d42b4a21e8dee5f
Jamin <jaminwebdev@gmail.com>
2022-09-07 18:17:07 -0700

    morse code dictionary added

--- morseCode.js
+++ morseCode.js
@@ -1,6 +1,64 @@
 // given a dictionary of morse code (MORSE_CODE)
 // implement a function that would take the morse code as input and return a decoded human-readable string
 
+const MORSE_CODE = {
+    '-.-.--': '!',
+  '.-..-.': '"',
+  '...-..-': '$',
+  '.-...': '&',
+  '.----.': '\'',
+  '-.--.': '(',
+  '-.--.-': ')',
+  '.-.-.': '+',
+  '--..--': ',',
+  '-....-': '-',
+  '.-.-.-': '.',
+  '-..-.': '/',
+  '-----': '0',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '---...': ':',
+  '-.-.-.': ';',
+  '-...-': '=',
+  '..--..': '?',
+  '.--.-.': '@',
+  '.-': 'A',
+  '-...': 'B',
+  '-.-.': 'C',
+  '-..': 'D',
+  '.': 'E',
+  '..-.': 'F',
+  '--.': 'G',
+  '....': 'H',
+  '..': 'I',
+  '.---': 'J',
+  '-.-': 'K',
+  '.-..': 'L',
+  '--': 'M',
+  '-.': 'N',
+  '---': 'O',
+  '.--.': 'P',
+  '--.-': 'Q',
+  '.-.': 'R',
+  '...': 'S',
+  '-': 'T',
+  '..-': 'U',
+  '...-': 'V',
+  '.--': 'W',
+  '-..-': 'X',
+  '-.--': 'Y',
+  '--..': 'Z',
+  '..--.-': '_',
+  '...---...': 'SOS'
+}
+
 const decodeMorse = (morseCode) => {
     function decodeMorseLetter(letter) {
         return MORSE_CODE[letter];
@@ -10,6 +68,8 @@ const decodeMorse = (morseCode) => {
     }
     return morseCode.trim().split('   ').map(decodeMorseWord).join(' ');
 }
+
+console.log(decodeMorse('.... . -.--   .--- ..- -.. .'))
 
 const decodeMorseRegex = (morseCode) => {
     return morseCode
