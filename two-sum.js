594d49739ddb4ef1982271d1130a03c48f311d88
Jamin <jaminwebdev@gmail.com>
2025-08-02 14:52:57 -0500

    kebab case

---
+++ two-sum.js
@@ -0,0 +1,20 @@
+function twoSum(nums, target) {
+  const numMap = {}; // Initialize an empty object to act as a hash map
+
+  for (let i = 0; i < nums.length; i++) {
+    const currentNum = nums[i];
+    const complement = target - currentNum;
+
+    // Check if the complement exists in the hash map
+    if (complement in numMap) {
+      // If found, return the index of the complement and the current index
+      return [numMap[complement], i];
+    }
+
+    // If the complement is not found, add the current number and its index to the map
+    numMap[currentNum] = i;
+  }
+
+  // If no solution is found (though the problem usually guarantees one)
+  return [];
+}
